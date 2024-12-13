c085a2c8ceab95e2f9facf8762329b4812f736e3
Jebin Joseph <jebin.joseph@outlook.com>
2024-12-12 22:52:18 -0500

    SJF algorithm file- Not finished

---
+++ src/operatingSystems/SJF_Algorithm.java
@@ -0,0 +1,19 @@
+package operatingSystems;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class SJF_Algorithm {
+	
+	/*This program Shortest Job algorithm in Process scheduling by getting input from user how many process are there
+	 * and burst time and arrival time 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] Headers = {"Process ID","AT","BT","CT","TAT","WT"};
+
+	}
+
+}
