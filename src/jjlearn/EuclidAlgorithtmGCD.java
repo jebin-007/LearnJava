9bd759f63a8d41c132f7e88f9ffa8e0d10cade28
Jebin Joseph <jebin.joseph@outlook.com>
2024-12-16 00:38:24 -0500

    Update to test.jj and euclid algorithm on 16 Dec 12.38am est

---
+++ src/jjlearn/EuclidAlgorithtmGCD.java
@@ -0,0 +1,22 @@
+package jjlearn;
+import java.util.Scanner;
+
+public class EuclidAlgorithtmGCD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first integer :");
+		a = sc.nextInt();
+		System.out.println("Enter the second integer :");
+		b = sc.nextInt();
+		System.out.println("So we need to find GCD(" + a + "," + b + ") ->");
+		
+	
+		
+
+	}
+
+}
--- src/jjlearn/TestJJ.java
+++ src/jjlearn/TestJJ.java
@@ -5,16 +5,11 @@ public class TestJJ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student jj1 = new Student("Jebin", 9886115783l);
-		jj1.getStudentInfo();
-		//New comment
-		//Commit from Zeph G14 on Dec 7, 2024
-		
-		//Comment from Tuf-Desk on Dec 7, 2024 12.50AM EST
-
-		// Comment from Zeph G14 on Dec 7, 2024 12.54AM EST
-
-		
+		int[] a = {34,56,27,98,19};
+		for(int i:a) {
+			System.out.println(i);
+			
+		}	
 
 
 	}
